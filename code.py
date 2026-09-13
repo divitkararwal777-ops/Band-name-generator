4f8b7cb235c46cedb03c803f1fa037078575c53d
divitkararwal777-ops <divitkararwal777@gmail.com>
2026-09-13 17:01:08 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,6 @@
+greetings = "Welcome to the Band Name Generator."
+print(greetings)
+question1 = input("What's the name of the city you grew up in?\n ")
+question2 = input("What's your pet's name?\n ")
+band_name = "Your band name could be " + question1 + " " + question2
+print(band_name)
