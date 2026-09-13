1c09894cef43fe1b062215eb309852924328362b
divitkararwal777-ops <divitkararwal777@gmail.com>
2026-09-13 17:14:34 +0530

    Migrate Band Name Generator to Streamlit

--- code.py
+++ code.py
@@ -1,6 +1,14 @@
-greetings = "Welcome to the Band Name Generator."
-print(greetings)
-question1 = input("What's the name of the city you grew up in?\n ")
-question2 = input("What's your pet's name?\n ")
-band_name = "Your band name could be " + question1 + " " + question2
-print(band_name)
+import streamlit as st
+
+st.title("🎸 Band Name Generator")
+
+st.write("Welcome to the Band Name Generator!")
+
+city = st.text_input("What's the name of the city you grew up in?")
+pet = st.text_input("What's the name of your pet?")
+
+if st.button("Generate Band Name"):
+    if city and pet:
+        st.success(f"Your band name could be {city} {pet} 🎵")
+    else:
+        st.warning("Please enter both your city and pet name.")
